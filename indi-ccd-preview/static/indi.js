2cacd5a31997e6b0ff9ddaa35d1d199296f356aa
Marco Gulino <marco@gulinux.net>
2017-01-03 19:41:49 +0000

    Improve INDI and INDIDevice objects

--- indi-ccd-preview/static/indi.js
+++ indi-ccd-preview/static/indi.js
@@ -3,9 +3,16 @@ var INDIDevice = function(devicename, properties) {
     this.properties = properties;
 
     this.reload = function(callback, devicename) {
-        var url = '/device/' + devicename === undefined ? this.currentDevice() : devicename + '/properties';
-        $.ajax(url, {success: this.__got_properties.bind(this, callback)});
+        $.ajax(this.__url(['properties']), {success: this.__got_properties.bind(this, callback)});
     }
+
+    this.get = function(property, callback) {
+        $.ajax(this.__url(['properties', property]), {success: callback});
+    };
+
+    this.set = function(property, value, callback) {
+       $.ajax(this.__url(['properties', property]), {method: 'PUT', data: {value: value}, success: callback}); 
+    };
 
     this.__got_properties = function(callback, data) {
         this.properties = data['properties'];
@@ -13,18 +20,27 @@ var INDIDevice = function(devicename, properties) {
             callback(this);
     }
 
+    this.__url = function(suburl) {
+        if( suburl === undefined)
+            suburl = [];
+        return ['/device', this.name].concat(suburl).join('/');
+    };
 };
 
 var INDI = function() {
+
+    this.devices = {};
 
     this.get_devices = function(callback) {
         $.ajax('/devices', {success: this.__got_devices.bind(this, callback)});
     };
 
+
     this.__got_devices = function(callback, data) {
-        this.devices = Object.keys(data).map(function(name){
-            return new INDIDevice(name, data[name]);
-        });
+        this.devices = {};
+        Object.keys(data).forEach(function(name){
+            this.devices[name] = new INDIDevice(name, data[name]);
+        }, this);
         if(callback)
             callback(this);
     };
