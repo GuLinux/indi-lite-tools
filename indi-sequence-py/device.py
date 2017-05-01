f2104e5a3f319d717cad77062d9ef38ff8e4493e
Marco Gulino <marco@gulinux.net>
2017-05-01 13:24:05 +0100

    Refactoring indi client

--- indi-sequence-py/device.py
+++ indi-sequence-py/device.py
@@ -1,4 +1,5 @@
 import PyIndi
+import time
 
 class Device:
     def __init__(self, name, indi_client):
@@ -12,17 +13,23 @@ class Device:
             self.device = self.indi_client.getDevice(self.name)
 
     def connect(self):
-        connect_property = None
-        while not connect_property:
-            connect_property = self.device.getSwitch("CONNECTION")
-
+        connect_property = self.__getControl('switch', 'CONNECTION')
         if self.device.isConnected():
             return
-
-        while not connect_property:
-            connect_property = self.device.getSwitch("CONNECTION")
         connect_property[0].s=PyIndi.ISS_ON  # the "CONNECT" switch
         connect_property[1].s=PyIndi.ISS_OFF # the "DISCONNECT" switch
         self.indi_client.sendNewSwitch(connect_property) # send this new value to the device
 
+
+    def __getControl(self, ctl_type, name):
+        ctl = None
+        attr = {
+            'number': 'getNumber',
+            'switch': 'getSwitch',
+            'text': 'getText',
+            'blob': 'getBlob'
+        }[ctl_type]
+        while not(ctl):
+            ctl = getattr(self.device, attr)(name)
+            time.sleep(0.1)
 
