033ffdcb9c1f0c0aa2b8e3763cfee4970a27d645
Marco Gulino <marco@gulinux.net>
2017-06-18 19:08:07 +0100

    better binning/settings management for dark/bias

--- pyindi_sequence/examples/gulinux-modular-with-callbacks/sequence_template.py
+++ pyindi_sequence/examples/gulinux-modular-with-callbacks/sequence_template.py
@@ -12,39 +12,52 @@ LIGHT_BIAS_COUNT = 50
 RED = {'exp': 1, 'count': 1}
 GREEN = RED
 BLUE = RED
+RGB_BINNING = 2
 RGB_DARK_COUNT = LIGHT_DARK_COUNT
 RGB_BIAS_COUNT = LIGHT_BIAS_COUNT
 
 
-add_prompt_step('Changing camera settings to RAW 16bit, bin 1x1, press Enter to confirm')
-sb.change_camera_settings(binning = 1, frame_type = 'FRAME_LIGHT' )
-sb.change_camera_settings(
-    roi = {'X': 0, 'Y': 0, 'WIDTH': 4656, 'HEIGHT': 3520},
-    compression_format = 'CCD_RAW',
-    controls = {'HighSpeedMode': 1, 'HardwareBin': 1},
-    switches = {'CCD_VIDEO_FORMAT': {'on': ['ASI_IMG_RAW16']}, 'CCD_CONTROLS_MODE': {'on': ['AUTO_BandWidth'], 'off': ['AUTO_Gain']} }
-)
+def change_settings(binning=1, frame_type='FRAME_LIGHT'):
+    height = 3520
 
+    # deal with INDI asi roi bug, solved in june 2017 - remove after rasbpberry daily builds get the updated solution
+    if binning == 3:
+        height = 3519
+    sb.change_camera_settings(binning = binning, frame_type = frame_type )
+    sb.change_camera_settings(
+        roi = {'X': 0, 'Y': 0, 'WIDTH': 4656, 'HEIGHT': height},
+        compression_format = 'CCD_RAW',
+        controls = {
+            'HighSpeedMode': 1,
+            'HardwareBin': 1,
+            'Gain': 140,
+        },
+        switches = {'CCD_VIDEO_FORMAT': {'on': ['ASI_IMG_RAW16']}, 'CCD_CONTROLS_MODE': {'on': ['AUTO_BandWidth'], 'off': ['AUTO_Gain']} }
+    )
+
+def dark_bias(name, dark_count, bias_count, dark_exposure = None, binning=1, auto_dark=False, skip_bias=False):
+    sb.add_message_step('Taking dark/bias frames - changing filter wheel to the Dark filter position')
+    sb.add_filter_wheel_step(filter_number=FILTER_DARK)
+    if not skip_bias:
+        change_settings(binning=binning, frame_type='FRAME_BIAS')
+        add_sequence('Bias-{0}-{1}x{1}'.format(name, binning), ASI_BIAS_EXPOSURE, count=bias_count, auto_dark = False)
+    if auto_dark or not dark_exposure:
+        sb.add_auto_dark(count = LIGHT_DARK_COUNT, name='Dark-{0}-{1}x{1}'.format(name, binning))
+    else:
+        sb.change_camera_settings(frame_type = 'FRAME_DARK')
+        add_sequence('Dark-{0}-{1}x{1}'.format(name, binning), dark_exposure, count=dark_count, auto_dark = False)
+
+
+add_prompt_step('Changing camera settings to RAW 16bit, bin 1x1, press Enter to confirm')
+change_settings(binning=1)
 sb.add_message_step('Changing filter wheel to Luminance')
 sb.add_filter_wheel_step(filter_number=FILTER_LUMINANCE)
 add_sequence('Light', exposure=LIGHT['exp'], count=LIGHT['count'])
 
-sb.add_message_step('Covering camera lens for dark frames')
-sb.add_filter_wheel_step(filter_number=FILTER_DARK)
-sb.change_camera_settings(frame_type = 'FRAME_BIAS')
-add_sequence('Bias-L-1x1', ASI_BIAS_EXPOSURE, count=LIGHT_BIAS_COUNT, auto_dark = False)
-sb.add_auto_dark(count = LIGHT_DARK_COUNT, name='Dark-L-1x1')
+# Bias/Dark in between light and RGB - comment this to take darks/bias all at the end
+dark_bias('Light', LIGHT_DARK_COUNT, LIGHT_BIAS_COUNT, binning=1, auto_dark=True)
 
-
-# Bin 3x3
-# sb.add_message_step('Setting bin to 3x3')
-# sb.change_camera_settings(frame_type = 'FRAME_LIGHT', binning = 3, roi={'X': 0, 'Y': 0, 'WIDTH': 4656, 'HEIGHT': 3519})
-
-# Bin 2x2
-sb.add_message_step('Setting bin to 2x2')
-sb.change_camera_settings(frame_type = 'FRAME_LIGHT', binning = 2, roi={'X': 0, 'Y': 0, 'WIDTH': 4656, 'HEIGHT': 3520})
-
-
+change_settings(binning=RGB_BINNING)
 sb.add_message_step('Changing filter wheel to the Red filter position')
 sb.add_filter_wheel_step(filter_number=FILTER_RED)
 add_sequence('Red', exposure=RED['exp'], count=RED['count'])
@@ -57,11 +70,18 @@ sb.add_message_step('Changing filter wheel to the Blue filter position')
 sb.add_filter_wheel_step(filter_number=FILTER_BLUE)
 add_sequence('Blue', exposure=BLUE['exp'], count=BLUE['count'])
 
-sb.add_message_step('Covering camera for Dark Frames')
-sb.add_filter_wheel_step(filter_number=FILTER_DARK)
-sb.change_camera_settings(frame_type = 'FRAME_BIAS')
-add_sequence('Bias-RGB-3x3', ASI_BIAS_EXPOSURE, count=RGB_BIAS_COUNT, auto_dark = False)
-sb.add_auto_dark(name='Dark-RGB-3x3', count = RGB_DARK_COUNT)
+
+# Bias/Dark with auto mode - comment this, and uncomment the lines below, to take all darks, including Luminance, after all sequences are done 
+dark_bias('RGB', RGB_DARK_COUNT, RGB_BIAS_COUNT, binning=RGB_BINNING, auto_dark=True)
+
+
+#dark_bias('Light', LIGHT_DARK_COUNT, LIGHT_BIAS_COUNT, binning=1, dark_exposure=LIGHT['exp'])
+#dark_bias('RGB', RGB_DARK_COUNT, RGB_BIAS_COUNT, binning=RGB_BINNING, dark_exposure=RED['exp'])
+##The followings are for different dark exposures between R, G, B
+##dark_bias('R', RGB_DARK_COUNT, RGB_BIAS_COUNT, binning=RGB_BINNING, dark_exposure=RED['exp'])
+##dark_bias('G', RGB_DARK_COUNT, RGB_BIAS_COUNT, binning=RGB_BINNING, dark_exposure=GREEN['exp'], skip_bias=True)
+##dark_bias('B', RGB_DARK_COUNT, RGB_BIAS_COUNT, binning=RGB_BINNING, dark_exposure=BLUE['exp'], skip_bias=True)
 
 print(sb)
 start_sequence()
+
