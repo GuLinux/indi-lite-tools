2afc7ce883275ed957b33bca3424f4a0b24467f6
Marco Gulino <marco@gulinux.net>
2017-07-02 14:04:49 +0100

    Display time in lcd_temp

--- raspberry_setup/home-settings/bin/lcd_temp.py
+++ raspberry_setup/home-settings/bin/lcd_temp.py
@@ -12,17 +12,25 @@ if len(sys.argv) > 1:
 
 print('Using {}s polling interval'.format(interval))
 
+def send_string(text):
+    return requests.put('http://localhost:5100/led', json={'text': text, 'brightness': 0, 'id': 'temp_humidity', 'overwrite': False})
 
 while True:
     try:
+        send_string(time.strftime('T%H.%M.%S', time.localtime()))
+        time.sleep(1)
+        send_string(time.strftime('T%H.%M.%S', time.localtime()))
+
         temp = requests.get('http://localhost:5100/temp_humidity')
         if temp.status_code == 200:
             temp_obj = temp.json()
             t = temp_obj['temperature']
             h = temp_obj['humidity']
             text = u'{0:.1f}c.{1:.1f}h'.format(t, h)
-            r = requests.put('http://localhost:5100/led', json={'text': text, 'brightness': 0, 'id': 'temp_humidity', 'overwrite': False})
+            time.sleep(1)
+            r = send_string(text)
             print('data: {}, response: {}'.format(temp.json(), r))
+            time.sleep(interval)
     except:
         traceback.print_exc()
-    time.sleep(interval)
+
