290e7ff2526120e676d7212e661c4a437c6ed0aa
Marco Gulino <marco@gulinux.net>
2018-02-02 18:32:07 +0000

    Add metods to retrieve all available cameras and filer wheels

--- pyindi_sequence/indiclient.py
+++ pyindi_sequence/indiclient.py
@@ -1,4 +1,7 @@
 import PyIndi
+from .device import Device
+from .camera import Camera
+from .filter_wheel import FilterWheel
 
     
 class INDIClient(PyIndi.BaseClient):
@@ -12,6 +15,12 @@ class INDIClient(PyIndi.BaseClient):
         self.setServer(address, port)
         self.callbacks = callbacks
         self.connectServer()
+
+    def cameras(self):
+       return [Camera(x, self) for x in self.__devices_by_interface('ccd')]
+
+    def filter_wheels(self):
+       return [FilterWheel(x, self) for x in self.__devices_by_interface('filter')]
 
     @property
     def device_names(self):
@@ -54,6 +63,11 @@ class INDIClient(PyIndi.BaseClient):
         callback = self.callbacks.get(name)
         if callback:
             callback(*args, **kwargs)
+
+    def __devices_by_interface(self, interface):
+        devices = [Device(x, self) for x in self.device_names]
+        return [x.name for x in devices if interface in x.interfaces]
+ 
 
     def __str__(self):
         return 'INDI client connected to {0}:{1}'.format(self.host, self.port)
