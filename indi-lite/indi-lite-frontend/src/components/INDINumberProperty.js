190f677300a1cd669d9ea7faa3b78b6777765a89
Marco Gulino <marco.gulino@gmail.com>
2018-03-26 22:47:54 +0100

    (WIP) parse sexagesimal formats

--- indi-lite/indi-lite-frontend/src/components/INDINumberProperty.js
+++ indi-lite/indi-lite-frontend/src/components/INDINumberProperty.js
@@ -3,13 +3,55 @@ import CommitPendingValuesButton from './CommitPendingValuesButton'
 import INDILight from './INDILight'
 import PRINTJ from 'printj'
 
+const sex2string = (format, value) => {
+    // %010.6m
+    // 0.0008388807046051948 => 0:00:03
+    // 22.2061 => 22:12:22
+
+
+    let formatSpecifiers = format.substring(1, format.indexOf('m')).split('.').map(x => parseInt(x));
+    let width = formatSpecifiers[0];
+    let fracSpecifier = formatSpecifiers[1];
+    let fracBase;
+
+    switch(fracSpecifier) {
+        case 9:
+            fracBase = 360000;
+            break;
+        case 8:
+            fracBase = 36000;
+            break;
+        case 6:
+            fracBase = 3600;
+            break;
+        case 5:
+            fracBase = 600;
+            break;
+        default:
+            fracBase = 60;
+            break;
+    }
+
+    let isNegative = parseInt(value) < 0;
+    console.log(`value: ${value}, format: ${format}, isNegative=${isNegative}, width=${width}, fracSpecifier=${fracSpecifier}, fracBase=${fracBase}`)
+
+    return value;
+
+//    value = Math.abs(value)
+//
+//
+//    let intPartRound = Math.round(value * fracBase);
+//    let remainder = intPartRound % fracBase
+//    let intPart = Math.trunc(intPartRound / fracBase);
+//    console.log(`intPartRound: ${intPartRound}, intPart: ${intPart}, remainder: ${remainder}`);
+//    
+//    return formatSpecifiers;
+}
 
 const formatValue = (value, displayValue) => {
     if(value.format.endsWith('m')) {
-        // TODO: return degrees formatting
-        return displayValue;
+        return sex2string(value.format, displayValue);
     }
-//    return displayValue;
     let formatted = PRINTJ.sprintf(value.format, displayValue)
     return formatted;
 }
