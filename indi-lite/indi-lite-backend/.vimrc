9292fb87cb63392e5bba975041f37f20a068391c
Marco Gulino <marco.gulino@gmail.com>
2018-04-03 12:09:55 +0100

    Rename/Remove project

--- indi-lite/indi-lite-backend/.vimrc
+++
@@ -1,2 +0,0 @@
-let g:ctrlp_working_path_mode = 'a'
-let g:ctrlp_user_command = 'cd %s && git ls-files -co --exclude-standard'
--- indi-lite/indi-lite-backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .server import Server
-from .sequence import Sequence
-from .sequence_item import SequenceItem
-from .device import Device
-from .indi_property import Property
-from .exceptions import *
-
